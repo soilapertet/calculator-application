48780a126ca0b0cf8607faf64cdd79459e26ee11
Soila Pertet <nicolepertet@gmail.com>
2021-12-08 18:45:34 +0300

    Create a subtraction function to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 var add = function(number1, number2) {
     return number1 + number2;
 };
- var number1 = parseInt(prompt("Enter a number here: "));
- var number2 = parseInt(prompt("Enter a number here: "));
- alert("The result of the addition is "+add(number1, number2)+".");
+var number1 = parseInt(prompt("Enter a number here: "));
+var number2 = parseInt(prompt("Enter a number here: "));
+alert("The result of the addition is "+add(number1, number2)+".");
 
-var subtract = function
+var subtract = function(number1, number2) {
+    return number1 -number2;
+}
+var number1 = parseInt(prompt("Enter a number here: "));
+var number2 = parseInt(prompt("Enter a number here: "));
+alert("The result of the subtraction is "+subtract(number1, number2)+".");
+
