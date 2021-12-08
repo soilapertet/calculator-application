42715237ce6935215545588ed4d85851e97051eb
Soila Pertet <nicolepertet@gmail.com>
2021-12-08 18:39:11 +0300

    Initialise the calculator application and link the 'add' function.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+};
+ var number1 = parseInt(prompt("Enter a number here: "));
+ var number2 = parseInt(prompt("Enter a number here: "));
+ alert("The result of the addition is "+add(number1, number2)+".");
+
+var subtract = function
